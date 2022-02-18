98c860ac9ca217b2215184dad163f9ac5dd9d009
Andre Kolber <andrekolber1@gmail.com>
2022-02-18 12:00:34 -0500

    completed exercise

---
+++ src/Sardines.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Message from './Message';
+
+const Sardines = () => {
+	return (
+		<div
+			className="Sardines"
+			style={{ backgroundImage: 'url(https://media.giphy.com/media/tVk4w6EZ7eGNq/giphy.gif)' }}
+		>
+			<Message>
+				<h1>you don't eat the sardines. the sardines eat you!</h1>
+				<h1>
+					<Link to="/">go back</Link>
+				</h1>
+			</Message>
+		</div>
+	);
+};
+
+export default Sardines;
